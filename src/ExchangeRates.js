fabce2113f20edafce221b6f28f89203d446b22a
gurusoft13 <49583931+gurusoft13@users.noreply.github.com>
2020-09-23 04:40:24 -0400

    init

---
+++ src/ExchangeRates.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { useQuery, gql } from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+    }
+  }
+`;
+
+const ExchangeRates = () => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p> loading... </p>;
+  if (error) {
+    console.log(error);
+    return <p> Error : </p>;
+  }
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency} : {rate}
+      </p>
+    </div>
+  ));
+};
+
+export default ExchangeRates;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import App from "./App";
 
-import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider
+} from "@apollo/client";
+import ExchangeRates from "./ExchangeRates";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io/",
+  cache: new InMemoryCache()
+});
+
+// useEffect(() => {
+//   client
+//     .query({
+//       query: gql`
+//         query GetRate {
+//           rates(currency: "USD") {
+//             currency
+//           }
+//         }
+//       `
+//     })
+//     .then((result) => console.log(result));
+// });
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2> My First Apollo app </h2>
+        <ExchangeRates />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+ReactDOM.render(<App />, rootElement);
